01738ca20e55f87db691b0fb7fcd440f4cf96c36
waraporn maensiri <wara.phon@odds.team>
2019-06-02 16:29:18 +0700

    inject http for get data for display in home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HTTP } from '@ionic-native/http/ngx';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  input: string;
+  listIndividual = [];
+  listCorporate = [];
+  constructor(private http: HTTP, private platform: Platform) { }
 
-  constructor() {}
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      this.search();
+    });
+  }
 
+  search() {
+    this.http.get(`http://www.mocky.io/v2/5cf0ac7f3000004b0000b9e1`, {}, {}).then(res => {
+      this.listIndividual = JSON.parse(res.data).results.filter(val => val.type === '01');
+      this.listCorporate = JSON.parse(res.data).results.filter(val => val.type === '02');
+    }).catch(err => {
+      console.error(err);
+    });
+
+  }
 }
